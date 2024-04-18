9eb2f00519b49cbc878fb47e79263dcd80fbf5d4
Khylko Andrii <hilko_a@itstep.academy>
2024-04-18 21:18:40 +0300

    add and incluede prod function

--- TestProjectPV311/TestProjectPV311.cpp
+++ TestProjectPV311/TestProjectPV311.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "sum.h"
+#include "prod.h"
 
 using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
     cout << "Sum = "<< sum(12,23)<< endl;
+    cout << "Prod = "<< prod(12,23)<< endl;
 }
